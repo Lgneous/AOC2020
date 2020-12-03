857d18b7faa048d0f1ef9551dcc024707f55b6e4
Julien Chedal-Anglay <32403402+Lgneous@users.noreply.github.com>
2020-12-03 18:00:58 +0100

    Added fseeks

--- Day1/solve.py
+++ Day1/solve.py
@@ -2,11 +2,13 @@ import itertools
 import sys
 
 def part1(input_file):
+  input_file.seek(0)
   inputs = itertools.combinations((int(s[:-1]) for s in input_file), 2)
   return next(x * y for (x, y) in inputs if x + y == 2020))
 
 
 def part2(input_file):
+  input_file.seek(0)
   inputs = itertools.combinations((int(s[:-1]) for s in input_file), 3)
   return next(x * y * z for (x, y, z) in inputs if x + y + z == 2020))
   
