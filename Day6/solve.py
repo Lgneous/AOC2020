82513f9c9fd05334da352eecd5260d0977e4cb61
Julien Chedal-Anglay <32403402+Lgneous@users.noreply.github.com>
2020-12-06 06:30:34 +0100

    Added Day6

---
+++ Day6/solve.py
@@ -0,0 +1,28 @@
+import sys
+from collections import Counter
+
+
+def part1(input_file):
+    input_file.seek(0)
+    content = input_file.read()
+    inputs = content.split("\n\n")
+    return sum(len(set(list(input.replace("\n", "")))) for input in inputs)
+
+
+def part2(input_file):
+    input_file.seek(0)
+    content = input_file.read()
+    inputs = content.split("\n\n")
+    return sum(
+        v == n_users
+        for input in inputs
+        for v in Counter(list(input.replace("\n", ""))).values()
+        if (n_users := len(input.split()))
+    )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            print(part1(f))
+            print(part2(f))
