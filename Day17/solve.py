1b92128e48612c40acc8c4c92ef034dbfe7c0bfd
Julien Chedal-Anglay <32403402+Lgneous@users.noreply.github.com>
2020-12-17 16:29:40 +0100

    Made Day17 solution more generic

--- Day17/solve.py
+++ Day17/solve.py
@@ -4,11 +4,22 @@ import numpy as np
 import scipy.ndimage.filters as snf
 
 
-def game_of_life_4d(board, kernel, n):
+def get_input(f):
+  f.seek(0)
+  content = f.read().split()
+  arr = []
+  for line in content:
+    arr.append([int(c == '#') for c in line.strip()])
+  return np.array(arr, dtype=int)
+
+
+def game_of_life_nd(board, n, dims):
+  kernel = np.ones((3,)*dims)
+  kernel[(1,)*dims] = 0
   y, x = board.shape
   M = n + 1
-  arr = np.zeros((M * 2, M * 2, M * 2 + y, M * 2 + x), dtype=int)
-  arr[M, M, M:M+y, M:M+x] = board
+  arr = np.zeros((M * 2,) * (dims - 2) + (M * 2 + y, M * 2 + x), dtype=int)
+  arr[((M,) * (dims - 2)) + (slice(M, M+y), slice(M, M+x))] = board
   for _ in range(n):
     neighbours = snf.convolve(arr, kernel, mode="constant", cval=0)
     new_gen = (arr == 0) & (neighbours == 3)
@@ -17,29 +28,6 @@ def game_of_life_4d(board, kernel, n):
   return arr.sum()
 
 
-def get_input(f):
-  f.seek(0)
-  content = f.read().split()
-  arr = []
-  for line in content:
-    arr.append([int(c == '#') for c in line.strip()])
-  return np.array(arr, dtype=int)
-  
-  
-def part1(f):
-  arr = get_input(f)
-  kernel = np.ones((1,3,3,3))
-  kernel[0,1,1,1] = 0
-  return game_of_life_4d(get_input(f), kernel, 6)
-
-
-def part2(f):
-  arr = get_input(f)
-  kernel = np.ones((3,3,3,3))
-  kernel[1,1,1,1] = 0
-  return game_of_life_4d(get_input(f), kernel, 6)
-
-
 with open(sys.argv[1]) as f:
-  print(part1(f))
-  print(part2(f))
+  print(game_of_life_nd(get_input(f), 6, 3))
+  print(game_of_life_nd(get_input(f), 6, 4))
